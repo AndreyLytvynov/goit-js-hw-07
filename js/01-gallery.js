904614be969f45427ff2def02359bc1bd9a06ae4
AndreyLytvynov <mrjastonehome@gmail.com>
2022-10-19 01:45:26 +0300

    Созданы 2 галереи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function CreateMarkupGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+galleryEl.innerHTML = CreateMarkupGallery(galleryItems);
+
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.className === "gallery__image") {
+    const modalGallery = basicLightbox.create(
+      `
+      <img src=${e.target.dataset.source} width="800" height="600">
+      `,
+      {
+        onShow: () => {
+          window.addEventListener("keydown", closeModal);
+        },
+        onClose: () => {
+          removeEventListener("keydown", closeModal);
+        },
+      }
+    );
+
+    modalGallery.show();
+
+    function closeModal(e) {
+      if (e.code === "Escape") {
+        modalGallery.close();
+      }
+    }
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function CreateMarkupGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+    </a>
+    `;
+    })
+    .join("");
+}
+
+galleryEl.innerHTML = CreateMarkupGallery(galleryItems);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  overlay: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
